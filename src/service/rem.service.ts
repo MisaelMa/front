3d3092ab403f3f3e7b3edea3b3f3ee889a04b89c
MisaelMa <amisael.amir.misael@gmail.com>
2021-05-04 21:22:00 -0500

    se removio un slah

--- src/service/rem.service.ts
+++ src/service/rem.service.ts
@@ -7,16 +7,16 @@ export async function setToke(token: string) {
 }
 
 export async function getRems(options?: AxiosRequestConfig) {
-  return await axios.get(`${url}/api/rem?`, { ...options });
+  return await axios.get(`${url}api/rem?`, { ...options });
 }
 
 export async function addRem(payload: Rem, options?: AxiosRequestConfig) {
-  return await axios.post(`${url}/api/rem/`, payload, { ...options }).then(data => data.data);
+  return await axios.post(`${url}api/rem/`, payload, { ...options }).then(data => data.data);
 }
 
 export async function updateRem(payload: Rem, options?: AxiosRequestConfig) {
   return await axios.patch(
-    `${url}/api/rem/${payload.id}`,
+    `${url}api/rem/${payload.id}`,
     payload,
     { ...options },
   );
