261452bdc45d75c32502bf0fd463b5903a4411e0
MisaelMa <amisael.amir.misael@gmail.com>
2021-05-04 21:18:35 -0500

    se agrego los servicios de la api

--- src/service/rem.service.ts
+++ src/service/rem.service.ts
@@ -1,21 +1,22 @@
 import axios, { AxiosRequestConfig } from 'axios';
 import { Rem } from '@/types/rem.type';
+import { url } from '@/config';
 
 export async function setToke(token: string) {
   axios.defaults.headers.common = { 'authorization': `Bearer ${token}` };
 }
 
 export async function getRems(options?: AxiosRequestConfig) {
-  return await axios.get(`http://localhost:4000/api/rem?`, { ...options });
+  return await axios.get(`${url}/api/rem?`, { ...options });
 }
 
 export async function addRem(payload: Rem, options?: AxiosRequestConfig) {
-  return await axios.post(`http://localhost:4000/api/rem/`, payload, { ...options }).then(data => data.data);
+  return await axios.post(`${url}/api/rem/`, payload, { ...options }).then(data => data.data);
 }
 
 export async function updateRem(payload: Rem, options?: AxiosRequestConfig) {
   return await axios.patch(
-    `http://localhost:4000/api/rem/${payload.id}`,
+    `${url}/api/rem/${payload.id}`,
     payload,
     { ...options },
   );
